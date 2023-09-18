1e131691a033b00aa87959f12e56fdeec40d5bd2
Xuradel <leagueofxuradel@gmail.com>
2023-09-17 20:07:12 -0600

    vercel

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n@_p4)zzeq%76@3hf#)@90lgpcy^-a3f^uemmq5@ol0)l&vwr8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
